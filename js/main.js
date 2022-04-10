baca79dfe9b1c2e80291cc94b15a5e9d95b6a5a0
Michael <m-silkov@inbox.ru>
2022-04-10 17:51:10 +0300

    add smooth scroll

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
-$(document).ready(function(){
+// slider
+$(document).ready(function () {
 	$(".owl-carousel").owlCarousel({
-		items:1,
+		items: 1,
 		nav: true,
 		dots: false,
 		loop: true,
-		autoplay:true,
-    	autoplayTimeout:3000,
-    	autoplayHoverPause:true
-	});	
+		autoplay: true,
+		autoplayTimeout: 3000,
+		autoplayHoverPause: true,
+	});
 });
+// smooth scroll
+$('a[href^="#"').on('click', function() {
 
+	let href = $(this).attr('href');
 
+	$('html, body').animate({
+			scrollTop: $(href).offset().top
+	});
+	return false;
+});
