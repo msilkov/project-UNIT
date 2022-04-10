e8fd6a5f4c31b92a67195a10be1ccfbfbd325f4f
Michael <m-silkov@inbox.ru>
2022-04-10 17:30:05 +0300

    upd project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		items:1,
+		nav: true,
+		dots: false,
+		loop: true,
+		autoplay:true,
+    	autoplayTimeout:3000,
+    	autoplayHoverPause:true
+	});	
+});
+
+
